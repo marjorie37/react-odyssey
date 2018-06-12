cd7c5a9441be4f82ef92af33b72e7b486007f3e2
Marjorie <marjorieme@hotmail.com>
2018-06-12 17:19:35 +0200

    ajout de la quête 040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const connection = require('/home/wilder/Documents/React/React-voyage/react-odyssey/back/helpers/db.js');
 const authRouter = express.Router();
 
 
 authRouter.post('/signup', (req, res) => {
-    res.send('I am in POST signup');
+    const user =
+        {
+            email: req.body.email,
+            password: req.body.password,
+            name: req.body.name,
+            lastname: req.body.lastname
+        }
+    const query = connection.query("INSERT INTO users SET ?", user, (error, results, fields) => {
+        if (error)
+            res.status(500).end();
+        res.end()
+    })
 });
-
-
 
 module.exports = authRouter;
--- front/src/index.js
+++ front/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core"
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const myTheme = createMuiTheme(theme)
+
+ReactDOM.render(<MuiThemeProvider theme={myTheme}><App /></MuiThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
